6746ba61f04e74cfd4814907e26c450b694bde27
stanislavlyalin <lyalinstas@gmail.com>
2018-10-12 23:06:04 +0300

    добавлена функция files для получения списка всех wav-файлов в указанном каталоге. Возвращает список структур, содержащих полный путь, id пользователя, пол, возраст и id записи.

---
+++ utils/files.py
@@ -0,0 +1,23 @@
+import os
+import glob
+import re
+
+def files(dir):
+    gender_dict = {'male': 0, 'female': 1}
+    age_dict = {'youth': 0, 'adult': 1, 'senior': 2}
+
+    wav_files = glob.glob(os.path.join(dir, '') + '*.wav')
+    filelist = []
+
+    for file in wav_files:
+        try:
+            res = re.findall('usr(\d{4})_(\w+)_(\w+)_(\d+)', file)[0]
+            user_id = int(res[0])
+            gender = gender_dict[res[1]]
+            age = age_dict[res[2]]
+            sample_id = int(res[3])
+            filelist.append([file, user_id, gender, age, sample_id])
+        except:
+            pass
+
+    return filelist
